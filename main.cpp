c0e806c6bade1eb9d8a57c1ad2f82a8fef833542
ralucaghit <91971987+ralucaghit@users.noreply.github.com>
2024-12-09 01:16:34 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,9 @@
 #include <iostream>
 #include <string>
 
-//*class Book {
-/*  std::string title;
-    int pages;
-
-public:
-    // Constructor
-    Book(const std::string& title, int pages) : title(title), pages(pages) {}
-
-    // Operatorul de atribuire
-    Book& operator=(const Book& other) {
-        // Copiem datele obiectului `other` în `*this`
-        title = other.title;
-        pages = other.pages;
-        return *this; // Returnăm o referință la obiectul curent
-    }
-
-    // Afișare
-    void display() const {
-        std::cout << "Book: " << title << ", Pages: " << pages << '\n';
-    }
-};
-*/
+//Item 10: Have assignment operators return a reference to *this.
+//Item 11: Handle assignment to self in operator=.
+//Item 12: Copy all parts of an object.
 
 class Tren {
     std::string nume;
@@ -32,12 +13,18 @@ public:
     Tren(const std::string& nume, int vagoane)
         : nume(nume), vagoane(vagoane) {}
 
-// Operatorul de atribuire
+//operatorul de atribuire
     Tren& operator=(const Tren& other) {
-        // Copiem datele obiectului `other` în `*this`
+         if (this == &other) { 
+            std::cout << "Ai incercat sa atribui unui obiect pe el insusi!!" << "\n";
+            //verific auto-atribuirea
+            return *this;
+        }
+
+        //copiez datele obiectului `other` in `*this`
         nume = other.nume;
         vagoane = other.vagoane;
-        return *this; // Returnăm o referință la obiectul curent
+        return *this; //returnez o referință la obiectul curent
     }
 
     void afiseaza() const {
@@ -49,8 +36,14 @@ int main() {
     Tren t1("IR 797", 4);
     Tren t2("R 13708", 6);
     
-    t1 = t2; // Atribuire
+    //item 10
+    /*t1 = t2; // Atribuire
     t1.afiseaza(); // Arată datele lui `t2` copiate în `t1`
+*/
+
+    //item 11
+    t1=t1;
+    t1.afiseaza();
 
     return 0;
 }
